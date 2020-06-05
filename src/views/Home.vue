023411c8d5d8f71b638ca71c51f2b5e7f7905a03
Elam Nabi <elam@nabi.nu>
2020-06-05 18:01:05 +0200

    Added Geolocation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <main>
+    <h1 id="coords">
+      Nuvarande koordinater:
+      <p>Latitude: {{ lat }} Longitude: {{ long }}</p>
+    </h1>
     <Camera @imageCapture="setImage" />
     <Canvas :imageCapture="imageCapture" />
   </main>
@@ -16,12 +20,26 @@ export default {
   },
   data() {
     return {
-      imageCapture: null
+      imageCapture: null,
+      lat: null,
+      long: null
     };
   },
   methods: {
     setImage(imageCapture) {
       this.imageCapture = imageCapture;
+    }
+  },
+  mounted() {
+    if (!navigator.geolocation) {
+      console.log("Your browser doesn't support geolocation");
+    } else {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.long = position.coords.longitude;
+        console.log("Latitude: ", this.lat);
+        console.log("Longitude: ", this.long);
+      });
     }
   }
 };
@@ -32,5 +50,15 @@ main {
   width: 100%;
   margin: auto;
   max-width: 560px;
+}
+
+#coords {
+  font-size: 3rem;
+  text-align: center;
+  margin-bottom: 2rem;
+
+  p {
+    font-size: 1rem;
+  }
 }
 </style>
